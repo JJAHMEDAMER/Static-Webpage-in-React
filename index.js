ee7db858b2f504c190e81e959b458fed9ef88951
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-27 04:32:09 +0200

    Reusable html

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
+
+function ReusableHTML (){
+    return <div>
+        <h1>Hi, welcome back</h1>
+        <h2>this is a custom html component</h2>
+        <h3>A reusable pice of html code</h3>
+    </div>
+}
+
+ReactDOM.render(<ReusableHTML/>, document.querySelector("#root"))
