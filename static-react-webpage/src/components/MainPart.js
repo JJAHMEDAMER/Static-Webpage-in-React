1654f384f4c77e0d5131c919ed1378a41799c551
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-01 13:53:40 +0200

    main content

---
+++ static-react-webpage/src/components/MainPart.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MainPart() {
+  return (
+    <div className="main-part">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was First released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has Well Over 100k stars on git hub</li>
+        <li>Is maintained by facebook</li>
+        <li>Powers thousands of apps</li>
+      </ul>
+    </div>
+  );
+}
