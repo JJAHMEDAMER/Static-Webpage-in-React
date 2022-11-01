75fdf65a45882ff991f7ec59a488a1e8526a3ef4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-01 13:53:50 +0200

    footer content

---
+++ static-react-webpage/src/components/footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Footer = () => (
+  <footer className="footer">
+    <p>2022 JJAHEMDAMER Development. All Rights Reserved</p>
+  </footer>
+);
+export default Footer;
